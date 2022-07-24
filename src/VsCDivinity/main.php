b8927abc340a469c7eb3d265bd6846add5697f61
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-24 18:08:28 -0400

    update main.php

--- src/VsCDivinity/main.php
+++ src/VsCDivinity/main.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace VsCDivinity;
+
+pocketmine\plugin\PluginBase;
+
+main class extends PluginBase {
+  
+  protected function onEnable(): void {
+    self::$instance = $this;
+    $this->getLogger()->notice("LobbyCore Activado");
+    (new Loader($this))->load();
+  }
+  
+  protected function onDisable(): void {
+    $this->getLogger()->notice("LobbyCore Desactivado")
+  }
