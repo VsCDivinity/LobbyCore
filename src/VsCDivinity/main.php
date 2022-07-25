4de1b85c0bfc4e0d1f40afa1a0f57d679edd4ff2
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-25 04:41:55 -0400

    update main.php

--- src/VsCDivinity/main.php
+++ src/VsCDivinity/main.php
@@ -21,4 +21,15 @@ class Main extends PluginBase {
     $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function (): void { $this->onScore(); }), 20);
   }
   
+  private function onScore() {
+        foreach ($this->getServer()->getOnlinePlayers() as $player) {
+        $scoreboard = new Scoreboard("TiTle", "ScoreBoard", [$player]);
+         $scoreboard->removeScoreboard();
+         $scoreboard->createScoreboard();
+         $scoreboard->addEntry($this->GetConfig()->get("Line-1"));
+         $scoreboard->addEntry("§7|§l§dPlayers: §r§l" .count ($this->getOnlinePlayers()));
+         $scoreboard->addEntry($this->GetConfig()->get("ServerName"));
+         $scoreboard->addEntry($this->GetConfig()->get("Line-2"));
+         }
+    }
 }
