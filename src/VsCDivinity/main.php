4115de77632c7d02ce8dd3c1da6b0a61261f17c0
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-24 21:47:46 -0400

    update main.php

--- src/VsCDivinity/main.php
+++ src/VsCDivinity/main.php
@@ -4,15 +4,19 @@ namespace VsCDivinity;
 
 pocketmine\plugin\PluginBase;
 
-main class extends PluginBase {
+class Main extends PluginBase {
   
   public function OnLoad(): void {
     $this->getLogger()->notice("LobbyCore Activado");
   }
+  
   public function OnEnable(): void {
     @mkdir($this->getDataFolder());
     $this->saveDefaultConfig();
     $this->getResource("config.yml");
+    
+    $NetworkName = $this->getConfig()->get("NetworkName");
+    $this->getServer()->getNetwork()->setname("");
   }
   
   
