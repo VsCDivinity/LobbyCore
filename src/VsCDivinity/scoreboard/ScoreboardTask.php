90d8665389732e385777b86bc45fb2d105f1b6b6
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-25 03:36:14 -0400

    update ScoreboardTask.php

--- src/VsCDivinity/scoreboard/ScoreboardTask.php
+++ src/VsCDivinity/scoreboard/ScoreboardTask.php
@@ -12,8 +12,10 @@ class ScoreboardTask extends Task {
     $scoreboard = new Scoreboard($this->GetConfig()->get("TiTle"), "ScoreBoard", [$player]);
     $scoreboard->removeScoreboard();
     $scoreboard->createScoreboard();
+    $scoreboard->addEntry($this->GetConfig()->get("Line-1"));
     $scoreboard->addEntry("§7|§l§dPlayers: §r§l" .count ($this->getOnlinePlayers()));
     $scoreboard->addEntry($this->GetConfig()->get("ServerName"));
+    $scoreboard->addEntry($this->GetConfig()->get("Line-2"));
   }
 
 }
