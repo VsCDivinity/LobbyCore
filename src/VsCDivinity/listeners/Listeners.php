c62b899b457c173ee0f4a5ae503b66223a6e8f16
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-25 01:14:02 -0400

    update Listeners.php

--- src/VsCDivinity/listeners/Listeners.php
+++ src/VsCDivinity/listeners/Listeners.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace VsCDivinity\Listeners;
+
+use VsCDivinity\EventListener;
+
+class Listeners {
+  
+  public function onEnable(): void {
+    $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
+  }
+  
+}
