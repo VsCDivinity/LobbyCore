d7c9f71c43ef5d45bc3c7775b0f932c8ef4f5170
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-25 04:15:12 -0400

    update EventListener.php No damage spawn

--- src/VsCDivinity/EventListener.php
+++ src/VsCDivinity/EventListener.php
@@ -2,6 +2,24 @@
 
 namespace VsCDivinity;
 
+use pocketmine\Server;
+use pocketmine\player\Player;
 use pocketmine\event\Listener;
+use pocketmine\event\player\entity\EntityDamageEvent;
+use pocketmine\event\player\PlayerExhaustEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
+
+class EventListevent implements Listener {
+  
+  public function onDamage(EntityDamageEvent $event):void {
+        if(!$event->getEntity() instanceof Player) return;
+        
+        $event->cancel();
+    }
+    
+    public function onHunger(PlayerExhaustEvent $event):void {
+        $event->cancel();
+    }
+
+}
