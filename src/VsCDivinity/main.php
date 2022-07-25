1d6e75d350bb32782b9ac94c45d4a43a1aeca772
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-24 21:40:31 -0400

    update main.php

--- src/VsCDivinity/main.php
+++ src/VsCDivinity/main.php
@@ -9,6 +9,11 @@ main class extends PluginBase {
   public function OnLoad(): void {
     $this->getLogger()->notice("LobbyCore Activado");
   }
+  public function OnEnable(): void {
+    @mkdir($this->getDataFolder());
+    $this->saveDefaultConfig();
+    $this->getResource("config.yml");
+  }
   
   
 }
