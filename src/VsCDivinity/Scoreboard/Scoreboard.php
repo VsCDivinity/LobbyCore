af82470d8f644750701885cf099a98b14411e65b
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-26 00:58:06 -0400

    Fix error scoreboard name

--- src/VsCDivinity/Scoreboard/Scoreboard.php
+++ src/VsCDivinity/Scoreboard/Scoreboard.php
@@ -15,13 +15,12 @@ class Scoreboard implements Listener {
   
   public function sendBasicScoreboard(Player $player): void
     {
-    $name = $event = getPlayer()->getName();
     $scoreboard = new ScoreboardAPI();
     $scoreboard->create($player, " §l§bLobby Core ");
     $scoreboard->setLine($player, 1, "§l");
     $scoreboard->setLine($player, 2, "§r----------------------");
     $scoreboard->setLine($player, 3, "§lLobbyCore.tebex.ui");
-    $scoreboard->setLine($player, 4, "§l§bName: §r§l" .$name);
+    $scoreboard->setLine($player, 4, "§l§bName: §r§l" .$player->getDisplayName);
     $scoreboard->setLine($player, 5, "----------------------");
   }
 
