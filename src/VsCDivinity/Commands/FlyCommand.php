6b627f4bedc7818361ca32af4d3705af134f28c7
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-25 23:32:57 -0400

    Fix error me olvide de poner ; XD

--- src/VsCDivinity/Commands/FlyCommand.php
+++ src/VsCDivinity/Commands/FlyCommand.php
@@ -20,7 +20,7 @@ class FlyCommand extends Command
     public function execute(CommandSender $sender, string $commandLabel, array $args): void
     {
         if (!$sender->hasPermission("fly.use")) {
-             $sender->sendMessage("§l[§r§c§l!§r§l] §r§c§lyou do not have permissions to use this command ")
+             $sender->sendMessage("§l[§r§c§l!§r§l] §r§c§lyou do not have permissions to use this command ");
             return;
         }
 
