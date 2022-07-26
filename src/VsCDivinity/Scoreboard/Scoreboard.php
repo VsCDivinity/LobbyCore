feb98bf6b60aad65aa494924735f5356163d3a83
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-25 21:26:51 -0400

    Rename ScoreboardTask.php to Scoreboard.php

---
+++ src/VsCDivinity/Scoreboard/Scoreboard.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace VsCDivinity\Scoreboard;
+
+use pocketmine\scheduler\Task;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\player\Player;
+
+class ScoreboardTask Task implements Listener {
+  
+  public function onJoin(PlayerJoinEvent $event): void {
+    $player = $event->GetPlayer();
+    self::sendBasicScoreboard($player);
+  }
+  
+  public function sendBasicScoreboard(Player $player): void
+    {
+    $scoreboard = new ScoreboardAPI();
+    $scoreboard->create($player, " §l§bLobby Core ");
+    $scoreboard->setLine($player, 1, "§l");
+    $scoreboard->setLine($player, 2, "§r----------------------");
+    $scoreboard->setLine($player, 3, "LobbyCore.tebex.ui");
+    $scoreboard->setLine($player, 4, "----------------------");{
+  }
+
+}
