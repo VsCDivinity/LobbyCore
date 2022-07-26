913e26f6c34ca8f30d35c377d860fd8279795d20
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-26 00:00:34 -0400

    update PingCommand.php

--- src/VsCDivinity/Commands/PingCommand.php
+++ src/VsCDivinity/Commands/PingCommand.php
@@ -1,1 +1,33 @@
+<?php
 
+namespace VsCDivinity\Commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+
+class PingCommand extends Command {
+  
+  public function __construct() {
+      
+      parent::__construct('ping', 'Ping Command');
+      $this->setPermission('Ping.use');
+      
+  }
+
+  public function execute(CommandSender $sender, string $commandLabel, array $args): void
+    {
+      if(!$sender->hasPermission("Ping.use")) {
+          $sender->sendMessage("§l[§r§c§l!§r§l] §r§c§lyou do not have permissions to use this command");
+          return;
+      }
+    
+      if (!$sender instanceof Player) {
+          return;
+      }
+    
+      if (!$sender->sendMessage("your ping is" .$sender->getPing()));
+    }
+    
+      
+}
