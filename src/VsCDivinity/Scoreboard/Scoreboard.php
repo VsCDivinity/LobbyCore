be0fb3d73b76ca45928262c196e8c120e3217ddb
VsCDivinity <108039806+VsCDivinity@users.noreply.github.com>
2022-07-26 00:35:47 -0400

    update Scoreboard.php

--- src/VsCDivinity/Scoreboard/Scoreboard.php
+++ src/VsCDivinity/Scoreboard/Scoreboard.php
@@ -15,12 +15,14 @@ class Scoreboard implements Listener {
   
   public function sendBasicScoreboard(Player $player): void
     {
+    $name = $event = getPlayer();
     $scoreboard = new ScoreboardAPI();
     $scoreboard->create($player, " §l§bLobby Core ");
     $scoreboard->setLine($player, 1, "§l");
     $scoreboard->setLine($player, 2, "§r----------------------");
     $scoreboard->setLine($player, 3, "§lLobbyCore.tebex.ui");
-    $scoreboard->setLine($player, 4, "----------------------");
+    $scoreboard->setLine($player, 4, "§l§bName: §r§l" .$name->getName());
+    $scoreboard->setLine($player, 5, "----------------------");
   }
 
 }
